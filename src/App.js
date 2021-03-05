260c5c66857fcc6e02e2661a31091d64e0b029ee
arthurseredaa <neonbetcy@gmail.com>
2021-03-05 11:29:47 +0200

    Create antd layout + staet for responsive design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import { Layout } from "antd";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+
+import "./App.css";
+import { Header } from "./components/Header/Header";
+import { AppContent } from "./components/Content/Content";
+import { useState } from "react";
 
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <div className="App">
-      App
-    </div>
+    <Layout style={{ height: "100vh", display: "flex" }}>
+      <Sidebar setCollapsed={setCollapsed} />
+      <Layout>
+        <Header collapsed={collapsed} />
+        <AppContent />
+      </Layout>
+    </Layout>
   );
 }
 
