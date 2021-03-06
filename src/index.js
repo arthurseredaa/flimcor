f3054f2cfda32e50e9ea344810e6132ca072e52b
arthurseredaa <neonbetcy@gmail.com>
2021-03-06 21:23:07 +0200

    BrowserRouter wrapper

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
 
 const client = new ApolloClient({
   uri: "https://dev.flimcor.com/graphql",
   cache: new InMemoryCache(),
-})
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
