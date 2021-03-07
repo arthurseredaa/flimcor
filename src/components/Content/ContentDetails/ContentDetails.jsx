7c725c40f0c7402762342cd2d9c638f1f224d633
arthurseredaa <neonbetcy@gmail.com>
2021-03-07 19:50:47 +0200

    Responsive details

--- src/components/Content/ContentDetails/ContentDetails.jsx
+++ src/components/Content/ContentDetails/ContentDetails.jsx
@@ -1,13 +1,15 @@
-import { Row, Col } from "antd";
+import { Row, Col, Button } from "antd";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import classes from "./ContentDetails.module.css";
 import { DetailsInfo } from "./DetailsInfo/DetailsInfo";
 import { OrderInfo } from "./OrderInfo/OrderInfo";
 import { ProductChar } from "./ProductChar/ProductChar";
+import CartFilledIcon from "../../../assets/icons/cart-filled.svg";
 
-export const ContentDetails = () => {
+export const ContentDetails = ({ collapsed }) => {
   const [state, setState] = useState({});
+  const [value, setValue] = useState(1);
   const { id } = useParams();
 
   useEffect(() => {
@@ -23,33 +25,92 @@ export const ContentDetails = () => {
     getData(id);
   }, [id]);
 
+  const handleDivid = () => {
+    if (value !== 0) {
+      setValue(value - 1);
+    }
+  };
+
   return (
     <div className={classes.detailsWrapper}>
       {state && (
         <div className={classes.container}>
-          <Row gutter={{ lg: 24, md: 12 }} style={{ textAlign: "center" }}>
-            <Col lg={7} md={12}>
-              {" "}
+          {collapsed && (
+            <Row gutter={24} className={classes.collapsedPrice}>
+              <Col span={10} style={{ fontSize: "26px", color: " #005BE4" }}>
+                {(parseFloat(state.price) * value).toFixed(1)}{" "}
+                <span style={{ fontSize: "12px", color: "rgba(0,0,0,.5)" }}>
+                  $
+                </span>
+              </Col>
+              <Col span={14} style={{ display: "flex", paddingRight: 0, justifyContent: "flex-end" }}>
+                <div className={classes.calcQunatity}>
+                  <span className={classes.divider} onClick={handleDivid}>
+                    -
+                  </span>
+                  <span className={classes.value}>
+                    <p>{value}</p>
+                  </span>
+                  <span
+                    className={classes.plus}
+                    onClick={() => setValue(value + 1)}
+                  >
+                    +
+                  </span>
+                </div>
+
+                <Button className={classes.buy}>
+                  <img src={CartFilledIcon} width="24px" />
+                </Button>
+              </Col>
+            </Row>
+          )}
+          <Row
+            gutter={{ lg: 24, md: 24 }}
+            style={{ textAlign: "center" }}
+            className={classes.productRow}
+          >
+            <Col lg={7} md={12} sm={24}>
               <img
                 src={state.image}
                 alt="product"
                 className={classes.productImage}
               />
             </Col>
-            <Col lg={10} md={12}>
-              <DetailsInfo {...state} />
+            <Col lg={10} md={12} sm={24}>
+              <DetailsInfo {...state} collapsed={collapsed} />
             </Col>
-            <Col lg={7} md={24}>
+            {state && state.characteristics && collapsed && (
+              <Col
+                lg={7}
+                md={24}
+                style={{ display: "flex", justifyContent: "center" }}
+              >
+                <ProductChar collapsed={collapsed} characteristics={state.characteristics} />
+              </Col>
+            )}
+            {!collapsed && (
+              <Col lg={7} md={24} sm={24}>
+                <OrderInfo {...state} />
+              </Col>
+            )}
+          </Row>
+          {state && state.characteristics && !collapsed && (
+            <Row gutter={24}>
+              <Col
+                lg={7}
+                md={24}
+                style={{ display: "flex", justifyContent: "center" }}
+              >
+                <ProductChar collapsed={collapsed} characteristics={state.characteristics} />
+              </Col>
+            </Row>
+          )}
+          {collapsed && (
+            <Col lg={7} md={24} sm={24}>
               <OrderInfo {...state} />
             </Col>
-          </Row>
-          <Row gutter={24}>
-            <Col lg={7} md={24} style={{display: "flex", justifyContent: "center"}}>
-              {state && state.characteristics && (
-                <ProductChar characteristics={state.characteristics} />
-              )}
-            </Col>
-          </Row>
+          )}
         </div>
       )}
     </div>
