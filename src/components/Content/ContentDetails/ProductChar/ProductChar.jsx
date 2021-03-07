bd1769074b4b2b9ea0584f64d89b0efc9f02bf8c
arthurseredaa <neonbetcy@gmail.com>
2021-03-07 19:51:43 +0200

    Responsive

--- src/components/Content/ContentDetails/ProductChar/ProductChar.jsx
+++ src/components/Content/ContentDetails/ProductChar/ProductChar.jsx
@@ -3,22 +3,33 @@ import { Card, Divider, Row, Col } from "antd";
 
 import classes from "./ProductChar.module.css";
 
-export const ProductChar = ({ characteristics }) => (
+export const ProductChar = ({ characteristics, collapsed }) => (
+  <>
+  {
+    collapsed && (
+      <div className={classes.collapsedBadge}>
+        Характеристики
+      </div>
+    )
+  }
   <Card className={classes.productCharWrapper} bordered={false}>
-    <h1 className={classes.title}>Характеристики товару</h1>
+    {
+      !collapsed && <h1 className={classes.title}>Характеристики товару</h1>
+    }
     {characteristics &&
       characteristics.map((char, index) => (
         <React.Fragment key={index}>
-        <Row key={index} gutter={24} style={{ textAlign: "left" }}>
-          <Col span={8} style={{ color: "#5B5B5B", fontSize: "13px" }}>
-            {char[0]}
-          </Col>
-          <Col span={16} style={{ fontSize: "14px" }}>
-            {char[1]}
-          </Col>
-        </Row>
-        <Divider style={{margin: "2px 0px 10px"}} />
+          <Row key={index} gutter={24} style={{ textAlign: "left" }}>
+            <Col span={8} style={{ color: "#5B5B5B", fontSize: "13px" }} className={classes.colItem}>
+              {char[0]}
+            </Col>
+            <Col span={16} style={{ fontSize: "14px" }} className={classes.colItem}>
+              {char[1]}
+            </Col>
+          </Row>
+          {!collapsed && <Divider style={{ margin: "2px 0px 10px" }} />}
         </React.Fragment>
       ))}
   </Card>
+  </>
 );
