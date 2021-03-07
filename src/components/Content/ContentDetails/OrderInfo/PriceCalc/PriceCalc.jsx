cbaf473a19838dcacb967176f02996d83c7a57c3
arthurseredaa <neonbetcy@gmail.com>
2021-03-07 19:51:20 +0200

    Create price calc component

---
+++ src/components/Content/ContentDetails/OrderInfo/PriceCalc/PriceCalc.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import CartFilledIcon from "../../../../../assets/icons/cart-filled.svg";
+import classes from "./PriceCalc.module.css";
+import { Row, Col, Input, Button } from "antd";
+
+export const PriceCalc = ({ price }) => {
+  const [value, setValue] = useState(1);
+
+  return (
+    <Row gutter={24}>
+      <Col
+        span={8}
+        style={{
+          color: "#4E9616",
+          fontSize: "24px",
+          display: "flex",
+          alignItems: "CENTER",
+        }}
+      >
+        <p style={{ margin: 0 }}>
+          {(parseFloat(price) * value).toFixed(1)}{" "}
+          <span
+            style={{
+              color: "#8C8C8C",
+              fontSize: "14px",
+            }}
+          >
+            {" "}
+            $
+          </span>
+        </p>
+      </Col>
+      <Col span={16}>
+        <div style={{ display: "flex" }}>
+          <Input
+            type="number"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            style={{ padding: "15px 16px", fontSize: "16px" }}
+          />
+          <Button className={classes.buyButton}>
+            <div
+              style={{
+                mask: `url(${CartFilledIcon}) center no-repeat`,
+                marginRight: "12px",
+                height: "15px",
+                width: "15px",
+                backgroundColor: "#fff",
+              }}
+            />
+            Buy
+          </Button>
+        </div>
+      </Col>
+    </Row>
+  );
+};
