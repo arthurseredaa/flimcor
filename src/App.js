cba921e45c850b80289bf478a9b851aca8037d40
arthurseredaa <neonbetcy@gmail.com>
2021-03-08 12:20:37 +0200

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import "./App.css";
+
 import { Layout } from "antd";
 import { Sidebar } from "./components/Sidebar/Sidebar";
-
-import "./App.css";
 import { Header } from "./components/Header/Header";
 import { AppContent } from "./components/Content/Content";
 import { useState } from "react";
